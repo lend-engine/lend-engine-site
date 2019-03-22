743f1b0646d9a1402450521f37dbbf7d13a68d13
Chris Tanner <christanner@CTMAC.local>
2019-03-22 14:46:08 +0000

    Hide loans from Crisp Video

--- src/AppBundle/Repository/CoreLoanRepository.php
+++ src/AppBundle/Repository/CoreLoanRepository.php
@@ -10,4 +10,5 @@ namespace AppBundle\Repository;
  */
 class CoreLoanRepository extends \Doctrine\ORM\EntityRepository
 {
+
 }
--- src/AppBundle/Services/LoanFeedService.php
+++ src/AppBundle/Services/LoanFeedService.php
@@ -3,6 +3,7 @@
 namespace AppBundle\Services;
 
 use Doctrine\ORM\EntityManager;
+use \Doctrine\Common\Collections\Criteria;
 
 class LoanFeedService
 {
@@ -18,7 +19,17 @@ class LoanFeedService
     {
         /** @var \AppBundle\Repository\CoreLoanRepository $loanRepo */
         $loanRepo = $this->em->getRepository('AppBundle:CoreLoan');
-        $loans = $loanRepo->findBy([], ['createdAt' => 'DESC'], 6);
+
+        // Don't show loans from CrispVideo
+        // Hard coded for now... open this up to something more scalable and DB related when the need arises
+        $crispVideo = $this->em->getReference(\AppBundle\Entity\Account::class, 423);
+
+        $criteria = new Criteria();
+        $criteria->where(Criteria::expr()->neq('library', $crispVideo));
+        $criteria->setMaxResults(6);
+
+        $loans = $loanRepo->matching($criteria);
+
         return $loans;
     }
 }
