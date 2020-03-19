1aa43335a363c7de72ea60ce77205df4b61e0d1b
Chris Tanner <christanner@CTMAC.local>
2020-03-19 19:40:02 +0000

    New version of user guide

--- app/DoctrineMigrations/Version20200319150352.php
+++ app/DoctrineMigrations/Version20200319150352.php
@@ -15,7 +15,7 @@ final class Version20200319150352 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('ALTER TABLE account ADD feed_opt_out TINYINT(1) DEFAULT NULL');
+        $this->addSql('ALTER TABLE account ADD feed_opt_out TINYINT(1) DEFAULT 0');
     }
 
     public function down(Schema $schema) : void
